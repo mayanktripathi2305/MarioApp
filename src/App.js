ff457701914a5e744e42ee571be8ad1e997d6782
Mayank <mayanktripathi2305@gmail.com>
2020-09-21 07:54:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <h1>Marioapp</h1>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
