1fa5d3f073d2f250f830fd3222343e6d7ec8765c
Mayank <mayanktripathi2305@gmail.com>
2020-09-22 23:38:40 +0530

    Recommit

---
+++ src/components/projects/Projectlist.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Projectlist = () => {
+  return (
+    <div className="container mt-4">
+      <div className="card mt-4">
+        <div class="card-body">
+          <h5 class="card-title">Project Title 1</h5>
+          <p class="card-text">Project Description 1</p>
+        </div>
+      </div>
+
+      <div className="card mt-4">
+        <div class="card-body">
+          <h5 class="card-title">Project Title 2</h5>
+          <p class="card-text">Project Description 2</p>
+        </div>
+      </div>
+
+      <div className="card mt-4">
+        <div class="card-body">
+          <h5 class="card-title">Project Title 3</h5>
+          <p class="card-text">Project Description 3</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projectlist;
