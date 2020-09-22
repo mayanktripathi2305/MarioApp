8ba09e2a4241909e48e7272881ae97d2dedb5e16
Mayank <mayanktripathi2305@gmail.com>
2020-09-22 08:27:32 +0530

    Modifed SignedoutNavLinks component

--- src/components/layout/Signedoutlinks.js
+++ src/components/layout/Signedoutlinks.js
@@ -7,7 +7,7 @@ const SignedinLinks = () => {
       <ul className="navbar-nav ml-auto">
         <li className="nav-item">
           <NavLink to="/" className="nav-link">
-            About
+            Sign Up
           </NavLink>
         </li>
         <li className="nav-item">
@@ -15,14 +15,11 @@ const SignedinLinks = () => {
             Login
           </NavLink>
         </li>
-        {/* <li className="nav-item">
-          <NavLink
-            to="/"
-            className="nav-link btn btn-outline-primary my-2 my-sm-0 rounded-circle"
-          >
-            NN
-          </NavLink> 
-        </li>*/}
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">
+            About
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
