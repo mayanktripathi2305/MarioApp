6bd3f33cbd648c970a4748772fe5f34de0712a2e
Mayank <mayanktripathi2305@gmail.com>
2020-09-22 23:36:49 +0530

    Created Notification component

---
+++ src/components/dashboard/Notifications.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Notifications = () => {
+  return (
+    <div className="container mt-4">
+      <div className="card mt-4">
+        <div className="card-body">
+          <h5 className="card-title">Notifications</h5>
+          <p className="card-text">Notification 1</p>
+          <p className="card-text">Notification 2</p>
+          <p className="card-text">Notification 3</p>
+          <p className="card-text">Notification 4</p>
+          <p className="card-text">Notification 5</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Notifications;
