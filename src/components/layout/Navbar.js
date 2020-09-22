353a04c8ab442971da2b7ee83e86144fbd819441
Mayank <mayanktripathi2305@gmail.com>
2020-09-22 08:19:24 +0530

    Added SignedinNavLinks component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
+import SignedinLinks from "./Signedinlinks";
 
 const Navbar = () => {
   return (
     <div className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link to="/">MarioApp</Link>
+        <Link to="/" className="navbar-brand h1">
+          MARIOAPP
+        </Link>
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <SignedinLinks />
       </div>
     </div>
   );
