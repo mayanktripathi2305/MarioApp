80708b6705b6340fb8d0fdfba3939f279c5bbc60
Mayank <mayanktripathi2305@gmail.com>
2020-09-22 23:35:12 +0530

    Added Dashboard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/layout/Navbar";
+import SignIn from "./components/auth/SignIn";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <NavBar />
+        <Switch>
+          <Route exact to="/" component={Dashboard}></Route>
+          <Route to="/SignIn" component={SignIn}></Route>
+        </Switch>
       </Router>
     </div>
   );
