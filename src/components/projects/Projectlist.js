ce8164752f17747a43e36ab6d576550485bcbc63
Mayank <mayanktripathi2305@gmail.com>
2020-09-23 14:34:50 +0530

    Added projectdetails component

--- src/components/projects/Projectlist.js
+++ src/components/projects/Projectlist.js
@@ -1,28 +1,14 @@
 import React from "react";
+import ProjectDetails from "./ProjectDetails";
 
 const Projectlist = () => {
   return (
     <div className="container mt-4">
-      <div className="card mt-4">
-        <div class="card-body">
-          <h5 class="card-title">Project Title 1</h5>
-          <p class="card-text">Project Description 1</p>
-        </div>
-      </div>
+      <ProjectDetails />
 
-      <div className="card mt-4">
-        <div class="card-body">
-          <h5 class="card-title">Project Title 2</h5>
-          <p class="card-text">Project Description 2</p>
-        </div>
-      </div>
-
-      <div className="card mt-4">
-        <div class="card-body">
-          <h5 class="card-title">Project Title 3</h5>
-          <p class="card-text">Project Description 3</p>
-        </div>
-      </div>
+      <ProjectDetails />
+      <ProjectDetails />
+      <ProjectDetails />
     </div>
   );
 };
