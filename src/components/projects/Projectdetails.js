79635e5b7e88e00be84d1836e453a4dbf3e80ced
Mayank <mayanktripathi2305@gmail.com>
2020-09-23 18:19:30 +0530

    Created Project Details Component

---
+++ src/components/projects/Projectdetails.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ProjectDetails = () => {
+  return (
+    <div className="card mt-4">
+      <div class="card-body">
+        <h5 class="card-title">Project Title 1</h5>
+        <p class="card-text">Project Description 1</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
