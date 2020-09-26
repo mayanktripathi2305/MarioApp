2373fa961626a48ab96ec1e16bcf0304bbe6b68e
Mayank <mayanktripathi2305@gmail.com>
2020-09-26 22:20:43 +0530

    Added Routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/layout/Navbar";
 import SignIn from "./components/auth/SignIn";
 import Dashboard from "./components/dashboard/Dashboard";
+import ProjectDetails from "./components/projects/ProjectDetails";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/projects/CreateProject";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <NavBar />
         <Switch>
-          <Route exact to="/" component={Dashboard}></Route>
-          <Route to="/SignIn" component={SignIn}></Route>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/Project/:id" component={ProjectDetails} />
+          <Route path="/SignIn" component={SignIn} />
+          <Route path="/SignUp" component={SignUp} />
+          <Route path="/CreateProject" component={CreateProject} />
         </Switch>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
